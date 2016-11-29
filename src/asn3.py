bb6a50c730962d11eaa9802033ccfe05948a6549
zigzaugg <joshuazaugg@gmail.com>
2016-11-29 16:58:03 -0600

    Final changes for project 3, additional data

--- src/asn3.py
+++ src/asn3.py
@@ -11,6 +11,7 @@ import Queue
 import pickle
 import math
 import operator
+import random
 
 # -----------SERVICE DEFINITION-----------
 # allcmd REQUEST DATA
@@ -241,166 +242,212 @@ if __name__ == "__main__":
     startPosition()
 
     # Number of nearest neighbors
-    k = 3
+    if len(sys.argv) > 1:
+        k = int(sys.argv[1])
+    else:
+        k = 3
 
     # initial = getSensorValue(1)
     
     init_time = time.time()
 
     # Train
-    trainingFile = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/trainingData.txt', 'ab')
-    # try:
-    #     #data = pickle.load('/home/ros_workspace/src/eecs301_grp_H/src/trainingData.pkl')
-    #     data = pickle.load(trainingFile)
-    #     print data
-    #    print "Data successfully retrieved"
-    #except:
-    data = []
+    if len(sys.argv) > 1 and int(sys.argv[2]) == 1:
+        trainingFile = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/trainingData.txt', 'ab')
+        # try:
+        #     #data = pickle.load('/home/ros_workspace/src/eecs301_grp_H/src/trainingData.pkl')
+        #     data = pickle.load(trainingFile)
+        #     print data
+        #    print "Data successfully retrieved"
+        #except:
+        data = []
 
-    # initLeft, initRight, initDMS, LorR, finalLeft, finalRight, finalDMS, gyro
-    c = 1
-    while (c):
-        attributes = [0]*9
+        # initLeft, initRight, initDMS, LorR, finalLeft, finalRight, finalDMS, gyro
+        c = 1
+        while (c):
+            attributes = [0]*9
+            attributes[0] = getSensorValue(leftIR_port)
+            attributes[1] = getSensorValue(rightIR_port)
+            attributes[2] = getSensorValue(DMS_port)
+
+            gyro, direction = turningLeft()
+            # gyro, direction = turningRight()
+
+            attributes[3] = direction
+            attributes[4] = getSensorValue(leftIR_port)
+            attributes[5] = getSensorValue(rightIR_port)
+            attributes[6] = getSensorValue(DMS_port)
+            attributes[7] = gyro
+
+            attributes[8] = input("How was that turn?  -2 for very underturned, 0 for correct, 2 for very overturned\n")
+
+            print attributes
+            data.append(attributes)
+            trainingFile.write(str(attributes) + '; \n')
+            c = input("Would you like to record another turn? 1 to continue\n")
+
+        #pickle.dump(data,'/home/ros_workspace/src/eecs301_grp_H/src/trainingData.pkl')
+        #pickle.dump(data,trainingFile)
+        trainingFile.close()
+
+        #shutdown()
+
+    # Test
+    if len(sys.argv) > 1 and int(sys.argv[2]) == 2:
+        # train = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/train.txt')
+        # test = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/test.txt')
+        orig_data = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/trainingData_withDiff.csv')
+
+        data = [0]*10
+        get_data = []
+        # test_data = []
+        data[0] = []
+        data[1] = []
+        data[2] = []
+        data[3] = []
+        data[4] = []
+        data[5] = []
+        data[6] = []
+        data[7] = []
+        data[8] = []
+        data[9] = []
+
+        for line in orig_data:
+            splitstring = line.split(',')
+            for i in range(len(splitstring)):
+                splitstring[i] = float(splitstring[i])
+            get_data.append(splitstring)
+
+        random.shuffle(get_data)
+        data[0] = get_data[:10]
+        data[1] = get_data[10:20]
+        data[2] = get_data[20:30]
+        data[3] = get_data[30:40]
+        data[4] = get_data[40:50]
+        data[5] = get_data[50:60]
+        data[6] = get_data[60:70]
+        data[7] = get_data[70:80]
+        data[8] = get_data[80:90]
+        data[9] = get_data[90:]
+
+        # print train_data
+
+        # for line in test:
+        #     splitstring = line.split(',')
+        #     for i in range(len(splitstring)):
+        #         splitstring[i] = float(splitstring[i])
+        #     test_data.append(splitstring)
+
+        # print test_data
+
+        avg_err = 0
+        for x in range(10):
+            test_set = []
+            train_set = []
+            test_set = data[x]
+            for j in range(10):
+                if j != x:
+                    train_set += data[j]
+
+            # print train_set[0]
+            # print '\n\n\n'
+            # print test_set[0]
+
+            labels = []
+            for test_pt in test_set:
+                dist_vec = []
+                for train_pt in train_set:
+                    err = 0
+                    for i in range(len(train_pt)-1):
+                        err += (test_pt[i] - train_pt[i])**2
+                    dist_vec.append((train_pt,math.sqrt(err)))
+                dist_vec.sort(key=operator.itemgetter(1))
+                
+                top_neighbors = []
+                for x in range(k):
+                    top_neighbors.append(dist_vec[x][0])
+
+                potential_label = {-2:0, -1:0, 0:0, 1:0, 2:0}
+
+                # labelSum = 0
+                for x in top_neighbors:
+                    # labelSum += x[-1]
+                    potential_label[x[-1]] += 1
+                # print potential_label
+
+                labels.append(max(potential_label.iteritems(), key=operator.itemgetter(1))[0])
+                # labels.append(round(labelSum/k)) 
+
+            print labels
+
+            correct = 0
+            i = 0
+            for test_pt in test_set:
+                if test_pt[-1] == labels[i]:
+                    correct += 1
+                i += 1
+
+            print (correct/float(len(test_set)))*100.0
+            avg_err += (correct/float(len(test_set)))*100.0
+
+        print avg_err/10
+
+    # Demo
+    if len(sys.argv) > 1 and int(sys.argv[2]) == 3:
+        train = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/trainingData.csv')
+
+        train_data = []
+
+        for line in train:
+            splitstring = line.split(',')
+            for i in range(len(splitstring)):
+                splitstring[i] = float(splitstring[i])
+            train_data.append(splitstring)
+
+        attributes = [0]*8
         attributes[0] = getSensorValue(leftIR_port)
         attributes[1] = getSensorValue(rightIR_port)
         attributes[2] = getSensorValue(DMS_port)
 
-        gyro, direction = turningLeft()
-        # gyro, direction = turningRight()
+        if len(sys.argv) > 2 and sys.argv[3] == 'L':
+            gyro, direction = turningLeft()
+        else:
+            gyro, direction = turningRight()
 
-        attributes[3] = direction
+        if direction == 'L':
+            attributes[3] = -100
+        else:
+            attributes[3] = 100
         attributes[4] = getSensorValue(leftIR_port)
         attributes[5] = getSensorValue(rightIR_port)
         attributes[6] = getSensorValue(DMS_port)
         attributes[7] = gyro
 
-        attributes[8] = input("How was that turn?  -2 for very underturned, 0 for correct, 2 for very overturned\n")
+        test_pt = attributes
 
-        print attributes
-        data.append(attributes)
-        trainingFile.write(str(attributes) + '; \n')
-        c = input("Would you like to record another turn? 1 to continue\n")
-
-    #pickle.dump(data,'/home/ros_workspace/src/eecs301_grp_H/src/trainingData.pkl')
-    #pickle.dump(data,trainingFile)
-    trainingFile.close()
-
-    #shutdown()
-
-    # # Test
-    # train = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/train.txt')
-    # test = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/test.txt')
-
-    # train_data = []
-    # test_data = []
-
-    # for line in train:
-    #     splitstring = line.split(',')
-    #     for i in range(len(splitstring)):
-    #         splitstring[i] = float(splitstring[i])
-    #     train_data.append(splitstring)
-
-    # print train_data
-
-    # for line in test:
-    #     splitstring = line.split(',')
-    #     for i in range(len(splitstring)):
-    #         splitstring[i] = float(splitstring[i])
-    #     test_data.append(splitstring)
-
-    # print test_data
-
-    # labels = []
-    # for test_pt in test_data:
-    #     dist_vec = []
-    #     for train_pt in train_data:
-    #         err = 0
-    #         for i in range(len(train_pt)-1):
-    #             err += (test_pt[i] - train_pt[i])**2
-    #         dist_vec.append((train_pt,math.sqrt(err)))
-    #     dist_vec.sort(key=operator.itemgetter(1))
+        dist_vec = []
+        for train_pt in train_data:
+            err = 0
+            for i in range(len(train_pt)-1):
+                err += (test_pt[i] - train_pt[i])**2
+            dist_vec.append((train_pt,math.sqrt(err)))
+        dist_vec.sort(key=operator.itemgetter(1))
         
-    #     top_neighbors = []
-    #     for x in range(k):
-    #         top_neighbors.append(dist_vec[x][0])
+        top_neighbors = []
+        for x in range(k):
+            top_neighbors.append(dist_vec[x][0])
 
-    #     potential_label = {'-2':0, '-1':0, '0':0, '1':0, '2':0}
+        potential_label = {-2:0, -1:0, 0:0, 1:0, 2:0}
 
-    #     labelSum = 0
-    #     for x in top_neighbors:
-    #         labelSum += x[-1]
-    #         # potential_label[x[-1]] += 1
-    #     # print potential_label
+        # labelSum = 0
+        for x in top_neighbors:
+            # labelSum += x[-1]
+            potential_label[x[-1]] += 1
+        print potential_label
 
-    #     labels.append(round(labelSum/k)) 
+        print max(potential_label.iteritems(), key=operator.itemgetter(1))[0]
 
-    # print labels
-
-    # correct = 0
-    # i = 0
-    # for test_pt in test_data:
-    #     if test_pt[-1] == labels[i]:
-    #         correct += 1
-    #     i += 1
-
-    # print (correct/float(len(test_data)))*100.0
-
-    # # Demo
-    # train = open('/home/rosuser/ros_workspace/src/eecs301_grp_H/src/train.txt')
-
-    # train_data = []
-    # test_data = []
-
-    # for line in train:
-    #     splitstring = line.split(',')
-    #     for i in range(len(splitstring)):
-    #         splitstring[i] = float(splitstring[i])
-    #     train_data.append(splitstring)
-
-    # attributes = [0]*9
-    # attributes[0] = getSensorValue(leftIR_port)
-    # attributes[1] = getSensorValue(rightIR_port)
-    # attributes[2] = getSensorValue(DMS_port)
-
-    # if len(sys.argv) > 1 and sys.argv[1] == 'L':
-    #     gyro, direction = turningLeft()
-    # else:
-    #     gyro, direction = turningRight()
-
-    # if direction == 'L':
-    #     attributes[3] = -100
-    # else:
-    #     attributes[3] = 100
-    # attributes[4] = getSensorValue(leftIR_port)
-    # attributes[5] = getSensorValue(rightIR_port)
-    # attributes[6] = getSensorValue(DMS_port)
-    # attributes[7] = gyro
-
-    # # attributes[8] = input("How was that turn?  -2 for very underturned, 0 for correct, 2 for very overturned\n")
-
-    # test_pt = attributes
-
-    # dist_vec = []
-    # for train_pt in train_data:
-    #     err = 0
-    #     for i in range(len(train_pt)-1):
-    #         err += (test_pt[i] - train_pt[i])**2
-    #     dist_vec.append((train_pt,math.sqrt(err)))
-    # dist_vec.sort(key=operator.itemgetter(1))
-    
-    # top_neighbors = []
-    # for x in range(k):
-    #     top_neighbors.append(dist_vec[x][0])
-
-    # potential_label = {'-2':0, '-1':0, '0':0, '1':0, '2':0}
-
-    # labelSum = 0
-    # for x in top_neighbors:
-    #     labelSum += x[-1]
-    #     # potential_label[x[-1]] += 1
-    # # print potential_label
-
-    # print round(labelSum/k)
+        # print round(labelSum/k)
 
 
     while not rospy.is_shutdown():
